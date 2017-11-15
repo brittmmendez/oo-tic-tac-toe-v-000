03fcd04730e8b7225506a70a056936394aba89af
Brittany Morris <brittmmendez@gmail.com>
2017-11-15 14:35:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,11 +24,11 @@ def display_board
 end
 
 def input_to_index(user_input)
-  @index=user_input.to_i - 1
+  user_input.to_i - 1
 end
 
-def move(player)
-  @board[@index] = player
+def move(index,player)
+  @board[index] = player
 end
 
 
